53a105ff7aebaab6807692bc8baf9cdcdf03e470
Matt Grau <matt.grau@gmail.com>
2015-03-10 15:36:07 -0600

    changed the makefile, added a quick test.sh script to benchmark

---
+++ bin/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo testing Simbuca with benchmark 2
+../simulations/simbuca ../simulations/bench2.sim
+echo removing any output files
+rm out_bench2_logfile.txt
+rm out_bench2_pAll.txt
